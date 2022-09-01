9dc947e2fc59e2c2edb612e77e6bb751a2288db9
MarcosCruzRodriguez <javokruger@gmail.com>
2022-09-01 15:47:42 -0300

    Borro 2-4

--- Ejercicio 2-3/src/Ejercicio 2-3.c
+++ Ejercicio 2-3/src/Ejercicio 2-3.c
@@ -1,11 +1,11 @@
 /*
-================================================================================
-Marcos Cruz Rodriguez, Div D
-Ejercicio 2-3
+ ================================================================================
+ Marcos Cruz Rodriguez, Div D
+ Ejercicio 2-3
 
-Usando el concepto de operadores lógicos booleanos en el condicional.
-Solicitar un número al usuario e informar si el número es par o impar.
-================================================================================
+ Usando el concepto de operadores lógicos booleanos en el condicional.
+ Solicitar un número al usuario e informar si el número es par o impar.
+ ================================================================================
 */
 #include <stdio.h>
 #include <stdlib.h>
