52491bb6f0fad964d4c5e6e2cb435b6a10162681
MarcosCruzRodriguez <javokruger@gmail.com>
2022-09-01 22:23:14 -0300

    Ejercicio 3-1

---
+++ Funciones/Ejercicio 3-1/src/Ejercicio 3-1.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Marcos Cruz Roriguez, Div D
+ Ejercicio 3-1 / funciones
+
+ Crear una función que muestre por pantalla el número entero que
+ recibe como parámetro.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	setbuf(stdout, NULL);
+
+
+
+	return 0;
+}
